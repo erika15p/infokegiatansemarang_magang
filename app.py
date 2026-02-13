caf48f139ae7ea99ee651559ec5ce1b44f78484e
Your Name <you@example.com>
2026-02-13 13:18:23 +0700

    ...

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, session
+from flask import jsonify
 from db import get_db
+from kalender_api import tambah_event as kegiatan
+from kalender_api import ambil_event_kalender
 
 app = Flask(__name__)
 app.secret_key = "infokegiatan-secret"
@@ -61,25 +64,80 @@ def tambah():
         return redirect("/login")
 
     nama = request.form.get("nama")
-    deskripsi = request.form.get("deskripsi")
     tanggal = request.form.get("tanggal")
     lokasi = request.form.get("lokasi")
+    deskripsi = request.form.get("deskripsi")
 
+    event_id = kegiatan(nama, deskripsi, tanggal, lokasi)
+    
     db = get_db()
     cur = db.cursor()
     cur.execute(
-        "INSERT INTO kegiatan (nama_kegiatan, deskripsi, tanggal, lokasi) VALUES (%s, %s, %s, %s)",
-        (nama, deskripsi, tanggal, lokasi)
+        "INSERT INTO kegiatan (nama_kegiatan, tanggal, lokasi, deskripsi, calender_event_id) VALUES (%s,%s,%s,%s,%s)",
+        (nama, tanggal, lokasi, deskripsi, event_id)
     )
     db.commit()
 
     return redirect("/admin")
+
+@app.route("/kalender")
+def kalender():
+    return render_template("kalender.html")
+
+@app.route("/api/kegiatan")
+def api_kegiatan():
+    db = get_db()
+    cur = db.cursor(dictionary=True)
+
+    cur.execute("""
+        SELECT id_kegiatan, nama_kegiatan, tanggal, lokasi, deskripsi
+        FROM kegiatan
+    """)
+
+    data = cur.fetchall()
+
+    events = []
+    for k in data:
+        events.append({
+            "id": k["id_kegiatan"],
+            "title": k["nama_kegiatan"],
+            "start": k["tanggal"].strftime("%Y-%m-%d"),
+            "location": k["lokasi"],
+            "description": k["deskripsi"]
+        })
+
+    return jsonify(events)
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/login")
 
+def sinkron_kalender_ke_db():
+    events = ambil_event_kalender()
+    db = get_db()
+    cur = db.cursor(dictionary=True)
+
+    for e in events:
+        event_id = e['id']
+        nama = e.get('summary', '')
+        deskripsi = e.get('description', '')
+        lokasi = e.get('location', '')
+        tanggal = e['start'].get('date')
+
+        cur.execute(
+            "SELECT id FROM kegiatan WHERE calendar_event_id=%s",
+            (event_id,)
+        )
+
+        if not cur.fetchone():
+            cur.execute(
+                """INSERT INTO kegiatan
+                (nama_kegiatan, tanggal, lokasi, deskripsi, calendar_event_id)
+                VALUES (%s,%s,%s,%s,%s)""",
+                (nama, tanggal, lokasi, deskripsi, event_id)
+            )
+            db.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
